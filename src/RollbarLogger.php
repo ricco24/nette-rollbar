070f4a4c08c05c588cf0c5b4b3ad495586c258a1
Samuel Kelemen <kelemen@modernweb.sk>
2014-05-26 10:48:54 +0200

    Add functions for nette/user
    Signed-off-by: Samuel Kelemen <kelemen@modernweb.sk>

--- src/RollbarLogger.php
+++ src/RollbarLogger.php
@@ -9,7 +9,14 @@ use \Tracy\Debugger as TDebugger;
  * Logger wit rollbar connect for Tracy.
  * Usage:
  * <code>
+ * // Init, best in bootstrap.php
  * \Kelemen\Rollbar\RollbarLogger::register($container->parameters['rollbar']);
+ * </code>
+ * 
+ * Use nette user:
+ * <code>
+ * // Call in presenter
+ * \Kelemen\Rollbar\RollbarLogger::setUser($this->getUser());
  * </code>
  */
 
@@ -48,5 +55,39 @@ class RollbarLogger extends \Tracy\Logger
         }
 		
 		return $response;
+	}
+	
+	/*************************** NETTE HELP FUNCTION **************************/
+	
+	/**
+	 * Set nette user to logger
+	 * @param \Nette\Security\User $user
+	 * @param string $usernameCol			username column name to get from identity
+	 * @param string $emailCol				email column name to get from identity
+	 */
+	public static function setUser(\Nette\Security\User $user, $usernameCol = 'username', $emailCol = 'email') {
+		\Rollbar::$instance->person = self::getRollbarUser($user, $usernameCol, $emailCol);
+	}
+	
+	/**
+	 * Set rollbar user from given nette user
+	 * @param \Nette\Security\User $user
+	 * @param string $usernameCol
+	 * @param string $emailCol
+	 * @return array
+	 */
+	private static function getRollbarUser(\Nette\Security\User $user, $usernameCol = 'username', $emailCol = 'email') {
+		if(!$user->isLoggedIn()) {
+			return array(
+				'id' => 'unlogged'
+			);
+		}			
+			
+		$userData = $user->getIdentity()->getData();
+		return array(
+			'id' => $user->getId(),
+			'username' => $userData[$usernameCol],
+			'email' => $userData[$emailCol]
+		);
 	}
 }
